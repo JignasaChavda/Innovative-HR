f28886de991878673b58465c46f060e3076b3490
JignasaChavda <jignasha@sanskartechnolab.com>
2025-02-27 17:33:31 +0530

    Added field for sub department in employee

---
+++ innovative_hr/innovative_hr/doctype/sub_department/sub_department.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, jignasha chavda and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Sub Department", {
+// 	refresh(frm) {
+
+// 	},
+// });
