43f4017fe96c4557bb4fd5534572118b348bdb59
JignasaChavda <jignasha@sanskartechnolab.com>
2025-03-03 11:45:24 +0530

    Updated working days calculation based on employment type

--- innovative_hr/override/salary_slip_override.py
+++ innovative_hr/override/salary_slip_override.py
@@ -10,6 +10,7 @@ class SalarySlip(TransactionBase):
         validate_active_employee(self.employee)
         self.validate_dates()
         self.check_existing()
+        
 
         if self.payroll_frequency:
             self.get_date_details()
@@ -20,6 +21,7 @@ class SalarySlip(TransactionBase):
         else:
             self.get_working_days_details(lwp=self.leave_without_pay)
 
+        self.set_new_working_days()
         self.set_salary_structure_assignment()
         if self.is_new():
             self.calculate_net_pay()
@@ -40,3 +42,95 @@ class SalarySlip(TransactionBase):
                     ),
                     alert=True,
                 )
+    def set_new_working_days(self):
+        
+        start_date = frappe.utils.getdate(self.start_date)
+        end_date = frappe.utils.getdate(self.end_date)
+        emp_type = frappe.get_value('Employee', self.employee, 'employment_type')
+        holiday_list = frappe.get_value('Employee', self.employee, 'holiday_list')
+        joining_date = frappe.db.get_value('Employee', self.employee, 'date_of_joining')
+        joining_date = frappe.utils.getdate(joining_date) if joining_date else None
+        relieving_date = frappe.db.get_value('Employee', self.employee, 'relieving_date')
+        currency = frappe.db.get_value('Employee', self.employee, 'salary_currency')
+        duration_in_days = (end_date - start_date).days
+        if joining_date:
+            joining_duration_days = (joining_date - start_date).days
+        if relieving_date:
+            relieving_duration_days = (end_date - relieving_date).days 
+
+        # Calculate working days based on the employement type
+        if emp_type in ["Worker"] and holiday_list:
+            holidays = frappe.get_all(
+                    "Holiday",
+                    filters={
+                        "parent": holiday_list,
+                        "holiday_date": ["between", [start_date, end_date]]
+                    },
+                    pluck="holiday_date"
+                )
+            duration_in_days = duration_in_days-len(holidays)
+               
+        if emp_type in ["Staff", "Staff Trainee"]:
+            duration_in_days = duration_in_days
+        
+
+
+        # Calculate absent days for mid-month employee joining
+        if joining_date and relieving_date and joining_date.month == relieving_date.month:
+            if start_date < joining_date <= end_date and start_date < relieving_date <= end_date:
+                if emp_type in ["Worker"] and holiday_list:
+                    joining_holidays = frappe.get_all(
+                        "Holiday",
+                        filters={
+                            "parent": holiday_list,
+                            "holiday_date": ["between", [start_date, joining_date]]
+                        },
+                        pluck="holiday_date"
+                    )
+                    relieving_holidays = frappe.get_all(
+                        "Holiday",
+                        filters={
+                            "parent": holiday_list,
+                            "holiday_date": ["between", [relieving_date, end_date]]
+                        },
+                        pluck="holiday_date"
+                    )
+                    absent_days = (joining_duration_days - len(joining_holidays)) + (relieving_duration_days - len(relieving_holidays))
+
+                elif emp_type in ["Staff", "Staff Trainee"]:
+                    absent_days = joining_duration_days + relieving_duration_days
+
+        else:
+            if joining_date and start_date < joining_date <= end_date:
+                if emp_type in ["Worker"] and holiday_list:
+                    joining_holidays = frappe.get_all(
+                        "Holiday",
+                        filters={
+                            "parent": holiday_list,
+                            "holiday_date": ["between", [start_date, joining_date]]
+                        },
+                        pluck="holiday_date"
+                    )
+                    absent_days = joining_duration_days - len(joining_holidays)
+
+                elif emp_type in ["Staff", "Staff Trainee"]:
+                    absent_days = joining_duration_days
+
+            if relieving_date and start_date < relieving_date <= end_date:
+                if emp_type in ["Worker"] and holiday_list:
+                    relieving_holidays = frappe.get_all(
+                        "Holiday",
+                        filters={
+                            "parent": holiday_list,
+                            "holiday_date": ["between", [relieving_date, end_date]]
+                        },
+                        pluck="holiday_date"
+                    )
+                    absent_days = relieving_duration_days - len(relieving_holidays)
+
+                elif emp_type in ["Staff", "Staff Trainee"]:
+                    absent_days = relieving_duration_days
+
+        
+        self.total_working_days = duration_in_days
+        self.absent_days = self.absent_days + absent_days
