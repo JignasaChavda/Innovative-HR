13ae15107e32d20fa57298d428be272823c40556
JignasaChavda <jignasha@sanskartechnolab.com>
2025-03-05 14:08:18 +0530

    updated custom field in employee

---
+++ innovative_hr/innovative_hr/doctype/contractor_company/contractor_company.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, jignasha chavda and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ContractorCompany(Document):
+	pass
